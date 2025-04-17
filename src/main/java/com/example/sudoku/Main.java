27847dd665017270a77a5f32b889ef4887cea25d
4rias01 <santiago.arias.rojas@correounivalle.edu.co>
2025-04-16 21:39:59 -0500

    Main class was documented

--- src/main/java/com/example/sudoku/Main.java
+++ src/main/java/com/example/sudoku/Main.java
@@ -8,11 +8,32 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Main class serving as the entry point for the Sudoku application.
+ * Extends the {@link javafx.application.Application} class to leverage
+ * JavaFX framework capabilities.
+ */
 public class Main extends Application {
+
+    /**
+     * Main method used to launch the JavaFX application.
+     *
+     * @param args command-line arguments passed to the application
+     * @author Sebastian Calvo
+     * @version 1.0
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Initializes and displays the primary stage of the application.
+     * This method is invoked automatically by the JavaFX runtime after
+     * the application has been launched.
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set
+     */
     @Override
     public void start(Stage primaryStage){
         new GameStage();
